0cbb88c279e9cd8871b2207b2629166b2355a947
JORGE WINDER <jorge.winder@gmail.com>
2018-08-17 20:13:03 -0500

    Captura inicial del proyecto

---
+++ View/Plantillas/menu.php
@@ -0,0 +1,39 @@
+
+
+<div class="navbar-fixed">    
+
+    <nav class="white">
+        <a href="#" data-target="slide-out" class="sidenav-trigger"><i id="imenu" class="material-icons">menu</i></a>
+        <a href="." class="brand-logo center" style="padding-top: 20px;"><img src="Recursos/img/ItalaMigone-LogoV2.png" alt="" class="responsive-img" width="260"/></a>
+        
+        <ul class="right hide-on-med-and-down">
+            <li><a href="#historias">Historias</a></li>
+            <li><a href="#contacto">Contacto</a></li>
+        </ul>
+
+        <ul class="left hide-on-med-and-down">
+            <li><a href="sesiones.php">Sesiones</a></li>
+            <li><a href="mi-portafolio.php">Mi portafolio</a></li>
+        </ul>                
+        
+    </nav>
+</div>
+
+
+<!--
+-->
+
+<ul id="slide-out" class="sidenav">
+  <li><a href="." class="brand-logo center" style="padding-top: 10px;"><img src="Recursos/img/ItalaMigone-LogoV2.png" alt="" class="responsive-img" width="120"/></a></li>
+  <li><div class="divider"></div></li>
+  <li><a href="sesiones.php">sesiones</a></li>
+  <li><div class="divider"></div></li>
+  <li><a class="waves-effect"  href="mi-portafolio.php">Portafolio</a></li>
+  <li><div class="divider"></div></li>
+  <li><a class="waves-effect"  href="#historias">Historias</a></li>
+  <li><div class="divider"></div></li>
+  <li><a class="waves-effect"  href="#contacto">Contacto</a></li>
+  <li><div class="divider"></div></li>
+</ul>
+
+      
