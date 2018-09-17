1c4946bb080d762bb14b5975edf918fdd9dc2e24
JORGE WINDER <jorge.winder@gmail.com>
2018-09-17 16:49:44 -0500

    Se agregó pixel de tag manager de google

--- View/Plantillas/cuerpo.php
+++ View/Plantillas/cuerpo.php
@@ -6,3 +6,22 @@
     height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
     <!-- End Google Tag Manager (noscript) -->
 
+    <!-- Facebook app -->
+    <script>
+    window.fbAsyncInit = function() {
+        FB.init({
+        appId      : '1555655211202166',
+        xfbml      : true,
+        version    : 'v3.1'
+        });
+        FB.AppEvents.logPageView();
+    };
+
+    (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "https://connect.facebook.net/en_ES/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+    </script>
